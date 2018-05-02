c00b0f9f2ac95015d0962a61e510826ba5537a32
Jack <khoicaominh.mmt@gmail.com>
2018-05-02 14:04:39 +0700

    Angular: update voting

--- src/app/home/components/voting.component.ts
+++ src/app/home/components/voting.component.ts
@@ -36,16 +36,23 @@ export class VotingComponent implements OnInit {
     onVoted(result: Object) {
         if (result['voted'] === true) {
             this.voting_result.candidates.push(result['candidateID']);
+            console.log(this.voting_result);
+        } else if (result['voted'] === false) {
+            const index = this.voting_result.candidates.indexOf(result['candidateID']);
+            if (index > -1) {
+                this.voting_result.candidates.splice(index, 1);
+            }
+            console.log(this.voting_result);
         }
     }
 
     onVoteToBlockchain() {
         /* Do check the list candidates is equal 4 or not */
-        if (this.voting_result.candidates.length > 4 ) {
-            this.snackBar.open('The maximum candidates you can vote for is 4 out of 6' , 'Got it', {
+        if (this.voting_result.candidates.length > 4 && this.voting_result.candidates.length <= 6) {
+            this.snackBar.open('The maximum candidates you can vote for are 4 out of 6' , 'Got it', {
                 duration: 30000,
             });
-        } else if (this.voting_result.candidates.length === 4 ) {
+        } else if (this.voting_result.candidates.length <= 4 ) {
            this._coreService.votingBlock(this.voting_result)
             .subscribe( result => {
                 if (result.hash) {
