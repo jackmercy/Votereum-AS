c3e9f93dea03ae40daf466c57a5759bfd29e8d2e
Jack <khoicaominh.mmt@gmail.com>
2018-07-20 08:57:55 +0700

    migrate rxjs 5 to 6 and update Angular 5.2 to 6

--- src/app/core/services/core.service.ts
+++ src/app/core/services/core.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+
+
+
+
 
 const httpOptions = {
     headers: new HttpHeaders({
