1d2ffd03a42daab823f1127123b55f7fbbce0560
Jack <khoicaominh.mmt@gmail.com>
2018-10-05 10:26:33 +0700

    update Ea UI

--- src/app/ea-admin/management/management.component.ts
+++ src/app/ea-admin/management/management.component.ts
@@ -89,25 +89,27 @@ export class ManagementComponent implements OnInit {
                 this.ballotInfo = data['ballotInfo'];
                 this.phaseInfo = data['phaseInfo'];
                 this.voterInfo = data['voterInfo'];
-                /* this.registeredVoterChartData = [this.voterInfo['registeredVoterCount']];
-                this.designatedTotal = this.voterInfo['registeredVoterCount']['value'];
-                this.voterChartData = [this.voterInfo['votedVoterCount'], this.voterInfo['fundedVoterCount']]; */
+                /* this.designatedTotal = this.voterInfo['registeredVoterCount']; */
+                this.designatedTotal = 60561589;
                 this.registeredVoterChartData = [
                     {
                         name: 'Number of registered citizen',
+                        /* value: this.voterInfo['registeredVoterCount'] */
                         value: 60561589
+
                     }
                 ];
-                this.designatedTotal = 60561589;
                 this.fundedVoterChartData = [
                     {
                         name: 'Funded citizen',
+                        /* value: this.voterInfo['fundedVoterCount'] */
                         value: 56901589
                     }
                 ];
                 this.votedVoterChartData = [
                     {
                         name: 'Voted citizen',
+                        /* value: this.voterInfo['votedVoterCount'] */
                         value: 44234173
                     }
                 ];
