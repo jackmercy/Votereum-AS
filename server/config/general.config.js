b8f700d6af9268114c24ea1eb3a068343097cef1
Jack <khoicaominh.mmt@gmail.com>
2018-12-13 07:19:43 +0700

    update mongodb config

--- server/config/general.config.js
+++ server/config/general.config.js
@@ -1,7 +1,8 @@
 export const GeneralConfig = {
     SECRET: 'blockchaindappethervoteistheshit',
     AS_HOST_URI : 'localhost:5000',
-    OBR_HOST_URI : 'localhost:3000',
+    OBR_HOST_URI : 'localhost:8000',
     BASE_API : '/api/',
-    MONGODB_CONNECTION_STRING : 'mongodb://localhost:27017/ether-vote-as'
+    /* MONGODB_CONNECTION_STRING : 'mongodb://localhost:27017/ether-vote-as' */
+    MONGODB_CONNECTION_STRING : 'mongodb+srv://admin:admin@ether-vote-as-gcytm.azure.mongodb.net/ether-vote-as?retryWrites=true'
 }
